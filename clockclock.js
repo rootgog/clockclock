3884438a2afbd20a1d086b5173bed6e34cee50f4
rootgog <dev.georgebishop@gmail.com>
2020-04-23 15:43:54 +0100

    multiple segments

--- clockclock.js
+++ clockclock.js
@@ -1,16 +1,13 @@
 export default class ClockClock {
-  constructor({ element, segments = 1, updateRate = 30 } = {}) {
+  constructor({ element, updateRate = 30 } = {}) {
     this.canvas = document.createElement("canvas");
     this.ctx = this.canvas.getContext("2d");
     element.appendChild(this.canvas);
-    this.canvas.height = 480;
-    this.canvas.width = 320;
+    this.canvas.height = 1000;
+    this.canvas.width = 1000;
     this.updateRate = updateRate;
 
-    this.segments = [];
-    for (let s = 0; s < segments; s++) {
-      this.segments.push(new Segment());
-    }
+    this.segments = {};
 
     this.numbers = [
       "111101101101111",
@@ -25,23 +22,32 @@ export default class ClockClock {
       "111101111001111",
     ];
   }
-  updateSegment(segment, number) {
-    this.segments[segment].set(this.numbers[number]);
+  addSegment({ name, radius = 20, pos = { x: 0, y: 0 } } = {}) {
+    this.segments[name] = new Segment({ radius, pos });
+  }
+  updateSegment(name, number) {
+    if (!this.segments.hasOwnProperty(name)) {
+      throw new Error("segment does not exist");
+    }
+    this.segments[name].set(this.numbers[number]);
     return this;
   }
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.segments.forEach((segment) => {
-      segment.draw(this.ctx);
-    });
+    for (const key in this.segments) {
+      if (this.segments.hasOwnProperty(key)) {
+        const segment = this.segments[key];
+        segment.draw(this.ctx);
+      }
+    }
     requestAnimationFrame(this.draw.bind(this));
   }
 }
 
 class Segment {
-  constructor({ raduis = 40, pos = { x: 0, y: 0 } } = {}) {
-    this.raduis = raduis;
+  constructor({ radius = 40, pos = { x: 0, y: 0 } } = {}) {
+    this.radius = radius;
     this.pos = pos;
     this.clocks = [];
 
@@ -54,10 +60,10 @@ class Segment {
         row.push(
           new Clock({
             pos: {
-              x: x * this.raduis + x * this.raduis + this.raduis,
-              y: y * this.raduis + y * this.raduis + this.raduis,
+              x: x * this.radius + x * this.radius + this.radius + this.pos.x,
+              y: y * this.radius + y * this.radius + this.radius + this.pos.y,
             },
-            raduis,
+            raduis: radius,
           })
         );
       }
@@ -80,76 +86,105 @@ class Segment {
       const row = arr[y];
       for (let x = 0; x < row.length; x++) {
         const cell = row[x];
-        if (cell == "1") {
-          let topleft = this.clocks[y][x];
-          let topright = this.clocks[y][x + 1];
-          let bottomleft = this.clocks[y + 1][x];
-          let bottomright = this.clocks[y + 1][x + 1];
 
-          const DOWN = 180;
-          const UP = 0;
-          const RIGHT = 90;
-          const LEFT = 270;
+        const zeroAbove = isZeroVal(x, y - 1);
+        const zeroBelow = isZeroVal(x, y + 1);
+        const zeroRight = isZeroVal(x + 1, y);
+        const zeroLeft = isZeroVal(x - 1, y);
 
-          const DIAL_1 = 1;
-          const DIAL_2 = 0;
+        let topleft = this.clocks[y][x];
+        let topright = this.clocks[y][x + 1];
+        let bottomleft = this.clocks[y + 1][x];
+        let bottomright = this.clocks[y + 1][x + 1];
 
-          function isZeroVal(x, y) {
-            if (arr[y] == undefined) return true;
-            if (arr[y][x] != "1") return true;
-          }
+        const DOWN = 180;
+        const UP = 0;
+        const RIGHT = 90;
+        const LEFT = 270;
+        const RESTING = 45;
 
-          function animateValue(value, index, to, time = 2) {
-            let updateRate = 30;
+        const DIAL_1 = "first";
+        const DIAL_2 = "second";
 
-            //need diff
-            let diff = to - value[index];
-            while (diff < -180) diff += 360;
-            while (diff > 180) diff -= 360;
-            if (diff == 0) {
-              diff = 360;
+        function isZeroVal(x, y) {
+          if (arr[y] == undefined) return true;
+          if (arr[y][x] != "1") return true;
+        }
+
+        function animateValue(obj, to, time = 0.6) {
+          let updateRate = 30;
+
+          //need diff
+          let a = obj.value;
+          let b = to;
+
+          let d = Math.abs(a - b) % 360;
+          let r = d > 180 ? 360 - d : d;
+
+          //calculate sign
+          let sign =
+            (a - b >= 0 && a - b <= 180) || (a - b <= -180 && a - b >= -360)
+              ? 1
+              : -1;
+          r *= sign;
+
+          let diff = r ? 180 + r : 360;
+
+          let step = diff / time / (1000 / updateRate);
+
+          let currentProgress = 0;
+          let startingVal = obj.value * 1;
+
+          let interval = setInterval(() => {
+            currentProgress += step;
+            obj.value = currentProgress + startingVal;
+            if (currentProgress > diff) {
+              obj.value = to;
+              obj.animating = false;
+              clearInterval(interval);
             }
+          }, 1000 / updateRate);
+        }
 
-            let step = diff / time / (1000 / updateRate);
-
-            let currentProgress = value[index];
-
-            let interval = setInterval(() => {
-              currentProgress += step;
-              value[index] += step;
-              if (currentProgress > diff) {
-                value[index] = to;
-                clearInterval(interval);
-              }
-            }, 1000 / updateRate);
-          }
-
-          const zeroAbove = isZeroVal(x, y - 1);
-          const zeroBelow = isZeroVal(x, y + 1);
-          const zeroRight = isZeroVal(x + 1, y);
-          const zeroLeft = isZeroVal(x - 1, y);
-
+        if (cell == "1") {
           if (zeroRight) {
             //zero to right
-            animateValue(topright.dials, DIAL_2, DOWN);
-            animateValue(bottomright.dials, DIAL_1, UP);
+            animateValue(topright.dials[DIAL_2], DOWN);
+            animateValue(bottomright.dials[DIAL_1], UP);
           }
           if (zeroLeft) {
             //zero to left
-            animateValue(topleft.dials, DIAL_1, DOWN);
-            animateValue(bottomleft.dials, DIAL_2, UP);
+            animateValue(topleft.dials[DIAL_1], DOWN);
+            animateValue(bottomleft.dials[DIAL_2], UP);
           }
 
           if (zeroBelow) {
             //zero below
-            animateValue(bottomright.dials, DIAL_2, LEFT);
-            animateValue(bottomleft.dials, DIAL_1, RIGHT);
+            animateValue(bottomright.dials[DIAL_2], LEFT);
+            animateValue(bottomleft.dials[DIAL_1], RIGHT);
           }
 
           if (zeroAbove) {
             //zero above
-            animateValue(topright.dials, DIAL_1, LEFT);
-            animateValue(topleft.dials, DIAL_2, RIGHT);
+            animateValue(topright.dials[DIAL_1], LEFT);
+            animateValue(topleft.dials[DIAL_2], RIGHT);
+          }
+        } else {
+          if (zeroBelow) {
+            if (zeroRight) {
+              animateValue(bottomright.dials[DIAL_2], RESTING);
+              animateValue(bottomright.dials[DIAL_1], RESTING);
+            }
+            if (zeroLeft) {
+              animateValue(bottomleft.dials[DIAL_1], RESTING);
+              animateValue(bottomleft.dials[DIAL_2], RESTING);
+            }
+          }
+          if (zeroAbove) {
+            if (zeroRight) {
+              animateValue(topright.dials[DIAL_2], RESTING);
+              animateValue(topright.dials[DIAL_1], RESTING);
+            }
           }
         }
       }
@@ -159,7 +194,18 @@ class Segment {
 }
 
 class Clock {
-  constructor({ pos = { x: 0, y: 0 }, raduis = 40, dials = [45, 45] } = {}) {
+  constructor({
+    pos = { x: 0, y: 0 },
+    raduis = 40,
+    dials = {
+      first: {
+        value: 45,
+      },
+      second: {
+        value: 45,
+      },
+    },
+  } = {}) {
     this.pos = pos;
     this.raduis = raduis;
     this.dials = dials;
@@ -169,18 +215,26 @@ class Clock {
      * circumferance
      */
     ctx.beginPath();
+    ctx.fillStyle = "whitesmoke";
     ctx.arc(this.pos.x, this.pos.y, this.raduis, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.fill();
     /**
      * dials
      */
-    this.dials.forEach((dial) => {
-      ctx.moveTo(this.pos.x, this.pos.y);
-      ctx.lineTo(
-        this.pos.x + Math.cos((Math.PI / 180.0) * (dial - 90)) * this.raduis,
-        this.pos.y + Math.sin((Math.PI / 180.0) * (dial - 90)) * this.raduis
-      );
-      ctx.stroke();
-    });
+    for (const key in this.dials) {
+      if (this.dials.hasOwnProperty(key)) {
+        const dial = this.dials[key];
+        ctx.beginPath();
+        ctx.fillStyle = "#000000";
+        ctx.moveTo(this.pos.x, this.pos.y);
+        ctx.lineTo(
+          this.pos.x +
+            Math.cos((Math.PI / 180.0) * (dial.value - 90)) * this.raduis,
+          this.pos.y +
+            Math.sin((Math.PI / 180.0) * (dial.value - 90)) * this.raduis
+        );
+        ctx.stroke();
+      }
+    }
   }
 }
